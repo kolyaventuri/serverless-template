7bd264e2d3b2a7cd67bcca8d50a93ea54fb8c2b3
Kolya Venturi <me@kolya.co>
2023-04-09 18:33:58 -0700

    Fix the serverless config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  externals: [nodeExternals()],
+  context: __dirname,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        },
+        exclude: /node_modules/
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts']
+  }
+};
